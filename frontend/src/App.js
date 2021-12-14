41bc76c6770e6855ce46ad71faabd5c5828d526a
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2021-12-15 00:30:09 +0600

    [ADD] Added frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
 
-function App() {
-	return <div className="app">Hello world</div>;
-}
+const App = () => {
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
