b79890d56c5489b8a40494342f0fcac9bd6aaba3
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2021-12-15 20:35:28 +0600

    [ADD] added home, cart & product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import ShopPage from "./pages/ShopPage";
+import { useState } from "react";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
 
 const App = () => {
+	const [showCart, setShowCart] = useState(true);
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-				</Routes>
+				<Header showCart={showCart} setShowCart={setShowCart} />
+				<Cart showCart={showCart} setShowCart={setShowCart} />
+				<main className="container">
+					<Routes>
+						<Route path="" element={<HomePage />} />
+						<Route path="product/:id" element={<ProductPage />} />
+						<Route path="cart" element={<CartPage />} />
+						<Route path="shop" element={<ShopPage />} />
+					</Routes>
+				</main>
+				<Footer />
 			</BrowserRouter>
 		</div>
 	);
